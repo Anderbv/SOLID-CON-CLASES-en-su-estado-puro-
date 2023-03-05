50c409a3fa15c6ab5eb7e245ce783979d461be36
Anderbv <hazard20199@gmail.com>
2023-03-05 00:53:38 -0500

    three commit

---
+++ InterfaceSegregation.ts
@@ -0,0 +1,75 @@
+//Many specific interfaces are better than one original purpose interface
+//Basicamente este principio se trata de que es mucho más facil tener multiples interfaces
+//que una sola interfaz que haga de todo
+
+interface PcVideoGame {
+    play(): void;
+    save(): void;
+    overWriteSave(): void;
+    load(): void;
+}
+
+class HardCoreVideoGame implements PcVideoGame {
+
+    play(): void {
+        console.log('Playing')
+    }
+
+    save(): void {
+        throw new Error("Can't save")
+    }
+
+    overWriteSave(): void {
+        throw new Error("can't overwrite save")
+    }
+
+    load(): void {
+        console.log('Can"t load')
+    }
+}
+
+//Por ejemplo en micraft tenes un modo que es hardcore
+//Ahi si vos moris, perdes todo, la partida, todo
+//en este caso PcVideoGame como es una interfaz "completa" con muchas propiedades
+//dentro de una sola interfaz, es solamente una interfaz que hace todo
+//No lo puedo divir entonces la unica forma de yo poder cambiarlo cual va a ser?
+//tirar un error
+
+//Yo no puedo guardar, sobreescribir, cargar una partida porque un harcorevideogame
+//no lo puede hacer, esa la idea
+
+//***************************************************************** */
+interface PcVideoGame2 {
+    play(): void;
+}
+
+interface VideoGameSaving {
+    save(): void;
+    overWriteSave(): void;
+    load(): void;
+}
+
+class BetterHardCoreVideoGame implements PcVideoGame2 {
+    play(): void {
+        console.log("")
+    }
+}
+
+class NormalVideoGame implements PcVideoGame2, VideoGameSaving {
+    play(): void {
+        console.log('jugando')
+    }
+
+    save(): void {
+        console.log('guardando partida')
+    }
+
+    overWriteSave(): void {
+        console.log('sobreescribienod partida')
+    }
+
+    load(): void {
+        console.log('cargandopartida')
+    }
+}
+
--- LiskovSubstitionP.ts
+++ LiskovSubstitionP.ts
@@ -1,2 +1,71 @@
-dsadasdadadadsadasdasd
-//dasdadasd
+// Subtypes must be substitutable for their base types;
+
+//Los subtipos tienen que poder ser sustituibles por subtipos base
+
+class LivingBeing { //Ser vivo
+    private name: string;
+    private age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public getName(): string {
+        return this.name;
+    }
+
+    public getAge(): number {
+        return this.age;
+    }
+
+    public eat() {
+        console.log("I'm eating");
+    }
+}
+
+class Human extends LivingBeing {
+    private job: string;
+
+    constructor(name: string, age: number, job: string) {
+        super(name, age);
+        this.job = job;
+    }
+
+    public getWork(): string {
+        return this.job;
+    }
+
+    public work() {
+        console.log("I'm working")
+    }
+}
+
+class Plant  extends LivingBeing {
+    private color: string;
+    constructor(name: string, age: number, color: string) {
+        super(name, age);
+        this.color = color;
+    };
+
+    public eat() {
+        throw new Error("I'm can't eating")
+    }
+};
+
+const LivingBeingArray: LivingBeing[] = [
+    new LivingBeing('Ander', 19),
+    new Human('Ander', 19, 'Jr programer'),
+        
+]
+
+LivingBeingArray.forEach(livingBeing => {
+    livingBeing.eat();
+});
+
+//En este caso una planta no puede comer
+//estamos rompiendo con que un subtipo puede ser sustituido por un subtipo base
+
+
+
+
